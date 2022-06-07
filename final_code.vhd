03f7ef619120ca86f1aa6de9204733fa4a41e635
Demet <63194475+dtumkaya@users.noreply.github.com>
2022-06-07 13:45:09 +0300

    Update final_code.vhd

--- final_code.vhd
+++ final_code.vhd
@@ -31,44 +31,49 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity final_code is
-	Port ( hundhz : in STD_LOGIC;
+	Port ( Clock : in STD_LOGIC;
 				  enter : in STD_LOGIC;
 				  input1 : in  STD_LOGIC_VECTOR (3 downto 0);
 				  input2 : in  STD_LOGIC_VECTOR (3 downto 0);
 				  input3 : in  STD_LOGIC_VECTOR (3 downto 0);
-				  result : out  STD_LOGIC_VECTOR (15 downto 0));
+				  result : out  STD_LOGIC_VECTOR (15 downto 0);
+				  mint:  out  STD_LOGIC_VECTOR (15 downto 0);
+				  maxt:  out  STD_LOGIC_VECTOR (15 downto 0));
 end final_code;
 
 architecture Behavioral of final_code is
 
 
 signal r : integer :=  0 ;
-
 signal count : integer := 3 ;
 signal average : integer := 0;
 signal result_temp : integer := 0;
-
-signal r_max : integer := 0;
+signal min_temp : integer := 0;
+signal max_temp : integer := 0;
 signal q : integer := 0;
 
+
+shared variable in1_int : integer ; 
+shared variable in2_int : integer ; 
+shared variable in3_int : integer ; 
+
+shared variable min_int : integer := 15;
+shared variable max_int : integer := 0;
+
 begin
 
-process(hundhz)
-
-variable in1_int : integer ; 
-variable in2_int : integer ; 
-variable in3_int : integer ; 
-variable result_int : integer ; 
+process(Clock)
+variable result_int : integer := 0 ; 
+variable n: integer := 0;
 
 begin
 
-if(rising_edge(hundhz)) then
-	
-	if (enter = '1') then
+if(rising_edge(Clock)) then
+
 		if(average = 0) then
 			in1_int := to_integer(unsigned(input1));
-			in2_int := to_integer(unsigned(input1));
-			in3_int := to_integer(unsigned(input1));
+			in2_int := to_integer(unsigned(input2));
+			in3_int := to_integer(unsigned(input3));
 			
 			result_int := in1_int + in2_int + in3_int;
 			average <= 1;
@@ -77,20 +82,56 @@ if(rising_edge(hundhz)) then
 			if (result_int > count) then
 				result_int := result_int - count;
 				q <= q+1;
-			elsif (result_temp = count) then
+			elsif (result_int = count) then
 				result_int := result_int - count;
 				q <= q+1;
 			else
 				result_temp <= q;
-				q<= 0;
+				q <= 0;
 				average <= 0;
 			end if;
 		end if;
-	end if;
 	
 end if;
+result <= std_logic_vector(to_unsigned(result_temp, 16));
+end process;
+
+process(Clock) -- not working, instead use r parameter to change (problem: clock!)
+begin
+if(falling_edge(Clock)) then
+			if(in1_int>max_int) then
+				max_int := in1_int;
+			elsif(in2_int>max_int) then
+				max_int := in2_int;
+			elsif (in3_int>max_int) then
+				max_int := in3_int;
+			else
+				max_temp <= max_int;
+			end if;
+			maxt <= std_logic_vector(to_unsigned(max_temp, 16));
+
+	
+end if;
+
+if(rising_edge(Clock)) then
+		
+			if(in1_int<min_int) then
+				min_int := in1_int;
+			elsif(in2_int<min_int) then
+				min_int := in2_int;
+			elsif (in3_int<min_int) then
+				min_int := in3_int;
+			else
+				min_temp <= min_int;
+			end if;
+			
+			mint <= std_logic_vector(to_unsigned(min_temp, 16));
+		
+end if;
+
+		
 end process;	
 
-result <= std_logic_vector(to_unsigned(result_temp, 16));
+
 
 end Behavioral;
