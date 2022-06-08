48b1161da740a54b0c173b1a09313853d65bc5b2
Demet <63194475+dtumkaya@users.noreply.github.com>
2022-06-08 22:33:02 +0300

    Update final_sim.vhd

--- final_sim.vhd
+++ final_sim.vhd
@@ -1,22 +1,12 @@
 --------------------------------------------------------------------------------
 -- Company: Koç University
 -- Engineer: Demet Tümkaya, Mahmut Esat Pişkin
+-- 
+-- Project Name:  ELEC204 - Magical LEDs of Statistics 
+-- Description: 	Designed to calculate maximum, minimum, range, median, 
+-- 					average for 3 inputs of 4 bits.
 --
--- Create Date:   10:22:33 06/07/2022
--- Design Name:   
--- Module Name:   
--- Project Name:  final_project
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
 -- VHDL Test Bench Created by ISE for module: final_code
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
 --
 -- Notes: 
 -- This testbench has been automatically generated using types std_logic and
@@ -28,21 +18,15 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
 ENTITY final_sim IS
 END final_sim;
  
 ARCHITECTURE behavior OF final_sim IS 
- 
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT final_code
     PORT(
          Clock : IN  std_logic;
-         enter : IN  std_logic;
          input1 : IN  std_logic_vector(3 downto 0);
          input2 : IN  std_logic_vector(3 downto 0);
          input3 : IN  std_logic_vector(3 downto 0);
@@ -57,7 +41,6 @@ ARCHITECTURE behavior OF final_sim IS
 
    --Inputs
    signal Clock : std_logic := '0';
-   signal enter : std_logic := '0';
    signal input1 : std_logic_vector(3 downto 0) := (others => '0');
    signal input2 : std_logic_vector(3 downto 0) := (others => '0');
    signal input3 : std_logic_vector(3 downto 0) := (others => '0');
@@ -68,17 +51,13 @@ ARCHITECTURE behavior OF final_sim IS
 	signal maxt : std_logic_vector(3 downto 0);
 	signal medt : std_logic_vector(3 downto 0);
 	signal ranget : std_logic_vector(3 downto 0);
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
  
    constant Clock_period : time := 100 ns;
  
 BEGIN
- 
 	-- Instantiate the Unit Under Test (UUT)
    uut: final_code PORT MAP (
           Clock => Clock,
-          enter => enter,
           input1 => input1,
           input2 => input2,
           input3 => input3,
@@ -97,14 +76,11 @@ BEGIN
 		Clock <= '1';
 		wait for Clock_period/10;
    end process;
- 
 
    -- Stimulus process
    stim_proc: process
    begin		
-      -- hold reset state for 100 ns.
 				
-		enter <= '1';
 		input1 <= "0001";
 		input2 <= "0010";
 		input3 <= "0011";
@@ -120,8 +96,6 @@ BEGIN
 		input1 <= "0111";
 		input2 <= "1000";
 		input3 <= "1001";
-
-      -- insert stimulus here 
 
       wait;
    end process;
